0f036345e0bcd4b66b724c81d825511131b7f38e
Cristian <ccastillon92@gmail.com>
2017-03-19 15:01:01 +0100

    Update Arguments_Optional.js

--- Arguments_Optional.js
+++ Arguments_Optional.js
@@ -1,24 +1,24 @@
-function addTogether (x) {
+function addTogether (first, second) {
   
- if (arguments.length === 1 && typeof arguments[0] === 'number'){  // If the argument is only a number.
+ if (arguments.length === 1 && typeof first === 'number'){  // If the argument is only a number.
    
-   return function (y) {  // return a function to provide another number.
+   return function (newSecond) {  // return a function to provide another number.
      
-     if (typeof arguments[0] === 'number'){  // If a number is provided then do the sum.
+     if (typeof newSecond === 'number'){  // If a number is provided then do the sum.
        
-       return x + y;  // If no number is provided the sum will return undefined.
+       return first + newSecond;  // If no number is provided the sum will return undefined.
      }
    };
  
  } else {  // In case the argument is formed by two numbers.
    
-   if (typeof arguments[0] !== 'number' || typeof arguments[1] !== 'number' ){  // If any member isn't a number.
+   if (typeof first !== 'number' || typeof second !== 'number' ){  // If any member isn't a number.
      
      return undefined;  // Return undefined.
      
    }
    
-   return arguments[0] + arguments[1];  // Either way, return the sum of both.
+   return first + second;  // Either way, return the sum of both.
  }
 }
 
